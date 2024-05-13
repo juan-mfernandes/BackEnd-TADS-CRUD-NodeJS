b8a8bef1eab33f30d756a66782abb594a652faec
juan-mfernandes <mfernandes.juan@gmail.com>
2024-05-13 11:40:00 -0300

    updating movies route

--- routes/movies.js
+++ routes/movies.js
@@ -1,22 +1,23 @@
 const express = require("express");
 const { findMovies, findById, createMovie, updateMovie, deleteMovie } = require("../project/db/movies");
 const moviesRouter = express.Router();
-
-moviesRouter.get("/movies", async(req, res) => {
+const { auth } = require('../project/middlewares/auth');
+    
+moviesRouter.get("/movies", auth, async(req, res) => {
     const rate = req.query.rate;
     const movies = await findMovies(rate);
     if (movies.length === 0) return res.status(404).json({message: "Rate Not Found."});
     res.json({movies});
 });
 
-moviesRouter.get("/movies/:id", async(req, res) => {
+moviesRouter.get("/movies/:id", auth, async(req, res) => {
     const id = Number(req.params.id);
     const movie = await findById(id);
     if (!movie) return res.status(404).json({message: `${id} id Not Found.`});
     res.json({movie});
 });
 
-moviesRouter.post("/movies", async(req, res) =>{
+moviesRouter.post("/movies", auth, async(req, res) =>{
     const data = req.body;
     if (!data.name || !data.rate){
         return res.status(400).json({message: "Movie name and rate are required."});
@@ -27,7 +28,7 @@ moviesRouter.post("/movies", async(req, res) =>{
     res.status(201).json({movie});
 });
 
-moviesRouter.put("/movies/:id", async(req, res) => {
+moviesRouter.put("/movies/:id", auth, async(req, res) => {
     const id = Number(req.params.id);
     const data = req.body;
     const movie = await updateMovie(id, data);
@@ -35,7 +36,7 @@ moviesRouter.put("/movies/:id", async(req, res) => {
     res.json({movie});
 });
 
-moviesRouter.delete("/movies/:id", async(req, res) => {
+moviesRouter.delete("/movies/:id", auth, async(req, res) => {
     const id = Number(req.params.id);
     await deleteMovie(id);
     if(!id) return res.status(404).json({message: `Movie ${id} not found.`});
